cde10bd870df650fcd563b8eb6af1a35cef24654
JoshCam <josh.lee.campbell@gmail.com>
2017-10-31 12:48:38 -0500

    formating & typo

--- firmware/CHICK/CHICK.ino
+++ firmware/CHICK/CHICK.ino
@@ -109,6 +109,7 @@ void setup() {
   // Ensure relay is off (active HIGH)
   digitalWrite(relayPin, LOW);
 
+}
 
 void loop() {
 
@@ -299,19 +300,19 @@ bool levelDeltaCheck(int p_initialLightReading, int p_lightReading)
 
 // this function will write a 2 byte integer to the eeprom at the specified address and address + 1
 void EEPROMWriteInt(int p_address, int p_value)
-     {
-     byte lowByte = ((p_value >> 0) & 0xFF);
-     byte highByte = ((p_value >> 8) & 0xFF);
+{
+  byte lowByte = ((p_value >> 0) & 0xFF);
+  byte highByte = ((p_value >> 8) & 0xFF);
 
-     EEPROM.write(p_address, lowByte);
-     EEPROM.write(p_address + 1, highByte);
-     }
+  EEPROM.write(p_address, lowByte);
+  EEPROM.write(p_address + 1, highByte);
+}
 
 // this function will read a 2 byte integer from the eeprom at the specified address and address + 1
 unsigned int EEPROMReadInt(int p_address)
-     {
-     byte lowByte = EEPROM.read(p_address);
-     byte highByte = EEPROM.read(p_address + 1);
+{
+  byte lowByte = EEPROM.read(p_address);
+  byte highByte = EEPROM.read(p_address + 1);
 
-     return ((lowByte << 0) & 0xFF) + ((highByte << 8) & 0xFF00);
-     }
+  return ((lowByte << 0) & 0xFF) + ((highByte << 8) & 0xFF00);
+}
